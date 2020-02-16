41a2a7df557abcc9712333aa5a655202d69985ab
marshall brock <marshallbrock@gmail.com>
2020-02-15 18:33:15 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,16 @@
-let cardOne = "queen";
-let cardTwo ="king";
-let cardThree ="queen";
-let cardFour ="king";
+let cards = ["queen","king","king","queen"];
+let cardsInPlay = [];
+let cardOne = cards[0];
+let cardTwo = cards[2]
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+if(cardsInPlay.length===2){
+	if(cardsInPlay[0]===cardsInPlay[1]){
+		alert("you found a match!");
+	}
+	else(alert("Sorry, try again."))
+}
+
+
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
